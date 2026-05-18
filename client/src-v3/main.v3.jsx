60698198c4f74bdcbb2ed7a40d6f86e802dc246f
Ryan Brook <rebrook@me.com>
2026-05-18 10:23:11 -0400

    Add Vercel Web Analytics to V3

--- client/src-v3/main.v3.jsx
+++ client/src-v3/main.v3.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import AppV3 from "./App.v3.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppV3 />
+    <Analytics />
   </StrictMode>
 );
